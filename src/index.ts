6d17a3be8609cf3cf38b7f0264654c09dce2cfe6
Gandara <120376565+Gandara247@users.noreply.github.com>
2023-05-30 09:09:11 -0300

    feat: add user repository

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, { Router } from "express";
 import cors from "cors";
-import { router } from "./routes";
+import  routes from "./routes";
 import { prisma } from "./database/db";
 
 async function main() {
     const app = express ();
     app.use(express.json());
-    app.use(router);
+    app.use(routes);
     app.use(cors())
     app.listen(3000, async () =>{
         console.log("server is running on https://127.0.0.1:3000");
