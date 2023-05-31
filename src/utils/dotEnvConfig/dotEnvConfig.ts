05ebbc585b1ab41c99b108cc90e6919be135e5eb
Gandara <120376565+Gandara247@users.noreply.github.com>
2023-05-31 08:31:23 -0300

    feat: add file .env.example

---
+++ src/utils/dotEnvConfig/dotEnvConfig.ts
@@ -0,0 +1,16 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const env: { [key: string]: string } = {
+    PORT: <string>process.env.PORT,
+    DATABASE_URL: <string>process.env.DATABASE_URL,
+    JWTSECRET: <string>process.env.JWTSECRET,
+    JWTREFRESHSECRET: <string>process.env.JWTREFRESHSECRET,
+};
+
+for (const property in env) {
+    if (!env[property])
+        throw Error(`Environment variable ${property} not set!`);
+}
+
+export default env;
