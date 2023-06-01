17e844744334664ae20678e24968078e75e76d93
Gandara <120376565+Gandara247@users.noreply.github.com>
2023-06-01 09:43:29 -0300

    feat: add schemas file

---
+++ src/middlewares/validateDTO/validateDTO.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Response, Request } from "express";
+import { AnySchema } from "yup";
+
+export default function validateDto(
+    schema: { body?: AnySchema; params?: AnySchema },
+) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            if (req.body.data) {
+                req.body.data - JSON.parse(req.body.data);
+            }
+            req.body = await schema.body?.validate(req.body);
+            req.params = await schema.params?.validate(req.params);
+            next();
+        } catch (error) {
+            next(error);
+        }
+    };
+}
