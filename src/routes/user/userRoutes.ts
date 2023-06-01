c5031f5af30d88748ea8b94cd9c70ab782adc455
Gandara <120376565+Gandara247@users.noreply.github.com>
2023-06-01 09:43:29 -0300

    feat: add schemas file

--- src/routes/user/userRoutes.ts
+++ src/routes/user/userRoutes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import Authentication from "../../middlewares/authentication/authentication";
+import userController from "../../controllers/userController";
 
 const userRoutes = Router();
+
+
+userRoutes.get("/users", Authentication.authAdmin, userController.index);
+userRoutes.post("/user")
 
 export default userRoutes;
