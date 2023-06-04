e40e5c9fc32dea5fae425383a9f27690b2b59269
Gandara <120376565+Gandara247@users.noreply.github.com>
2023-06-03 23:59:29 -0300

    feat: CRUD user done

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,5 @@
-import express from "express";
-import cors from "cors";
-import  routes from "./routes";
-import { prisma } from "./database/db";
+import { env } from "process";
+import api from "./utils/api/api";
 
-async function main() {
-    const app = express ();
-    app.use(express.json());
-    app.use(routes);
-    app.use(cors())
-    app.listen(3000, async () =>{
-        console.log("server is running on https://127.0.0.1:3000");
 
-        try {
-            await prisma.$connect();
-            console.log("🎲 Successfully connected to the database!");
-            
-        }catch(error){
-            console.log("🤬 Faile connecting to the database!");
-            
-        }
-        
-    });
-}
-
-main().catch((error) => {
-    console.log("🤬 erro!");
-    console.log(error);    
-    
-});
-
+api.listen(env.PORT, () => console.log("🎲 Successfully connected to the database!"));
--- src/routes/user/userRoutes.ts
+++ src/routes/user/userRoutes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import Authentication from "../../middlewares/authentication/authentication";
 import userController from "../../controllers/userController";
+import validateDto from "../../middlewares/validateDTO/validateDTO";
+import { userSchema } from "../../middlewares/schemas/userSchema";
 
 const userRoutes = Router();
 
 
-userRoutes.get("/users", Authentication.authAdmin, userController.index);
-userRoutes.post("/user")
+userRoutes.get("/user", Authentication.authAdmin, userController.index); //ok
+userRoutes.post("/user", validateDto(userSchema.createUser), userController.createUserClient); //ok
+userRoutes.post("/admin", Authentication.authAdmin, validateDto(userSchema.createUser), userController.createUserAdmin); //ok
+userRoutes.post("/user/login", validateDto(userSchema.login), userController.login); //ok
+userRoutes.get("/user/logout", userController.logout); //ok
+userRoutes.get("/user/:email", Authentication.authClient, Authentication.validateUserRole, userController.fetchByEmail); //ok
+userRoutes.put("/user/:email", Authentication.authClient, Authentication.validateUserRole, validateDto(userSchema.updateUser), userController.update); //ok
+userRoutes.delete("/user/:email", Authentication.authClient, Authentication.validateUserRole, validateDto(userSchema.deleteUser), userController.deleteUser) //OK
 
 export default userRoutes;
