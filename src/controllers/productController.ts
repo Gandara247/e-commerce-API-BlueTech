52d5c31a4c551baf524e29454eedb8e91e0d0539
AbnerPapassoni <abnner0@gmail.com>
2023-06-09 16:21:46 -0300

    added products CRUD

---
+++ src/controllers/productController.ts
@@ -0,0 +1,68 @@
+import { NextFunction, Response, Request } from "express";
+import apiError from "../utils/api/apiError";
+import Product from "../repositories/productsRepository";
+
+export default class productController {
+
+	static async listProducts(req: Request, res: Response, next: NextFunction) {
+		try {
+			const products = await Product.fetchProducts();
+			return res.status(200).json(products);
+		} catch (error) {
+			next(error);
+		}
+	}
+
+    static async fetchProductById(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id } = req.params;
+            const product = await Product.fetchProductId(parseInt(id));
+            if (!product) { throw new apiError(404, "🔎 Product not found for this ID.") };
+            return res.status(200).json(product);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async createProduct(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { name, description, price, inventory } = req.body;
+            const product = await Product.createProduct({
+                name,
+                description,
+                price,
+                inventory,
+            });
+            return res.status(201).json(product)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async updateProduct(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id } = req.params;
+            const { name, description, price, inventory } = req.body;
+            const product = await Product.updateProduct({
+                id:(Number(id)),
+                name,
+                description,
+                price,
+                inventory
+            });
+            return res.status(200).json(product);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async deleteProduct(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id } = req.params;
+            await Product.deleteProduct(parseInt(id));
+            return res.sendStatus(204)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRoutes from "./routes/user/userRoutes";
+import productRoutes from "./routes/products/productRoutes";
 
 
  const routes = Router();
 
 routes.use(userRoutes)
+routes.use(productRoutes)
 
 routes.get("/", async(req, res) =>{
     return res.sendStatus(200).json({"msg":"👌🏿 Application runnig successfully!" });
