ca954d32eddbe0fc9670521e6d5925b0dbb5e1ab
Gandara <120376565+Gandara247@users.noreply.github.com>
2023-07-03 03:45:20 -0300

    feat: updated  secret file

--- src/utils/storeImages.ts
+++ src/utils/storeImages.ts
@@ -5,6 +5,9 @@ import { Storage } from '@google-cloud/storage';
 import { format } from 'util';
 
 
+let storage: Storage;
+let bucket: any;
+
 async function getSecret() {
   const client = new SecretManagerServiceClient();
   const name = 'projects/third-zephyr-391506/secrets/gandarasecret/versions/latest';
@@ -12,33 +15,32 @@ async function getSecret() {
   if (version.payload && version.payload.data) {
     const secretValue = version.payload.data.toString();
     const credentials = JSON.parse(secretValue);
-    const storage = new Storage({ credentials });
-    const bucket = storage.bucket(env.GCLOUD_STORAGE_BUCKET);
-
-    async function storeImages(files: Array<Express.Multer.File>) {
-      let images: string[] = [];
-      if (!files) throw new apiError(400, "No image uploaded!");
-      for (const image of files) {
-        const blob = bucket.file(image.originalname);
-        const blobStream = blob.createWriteStream();
-        blobStream.on('error', (error) => {
-          throw error;
-        });
-        blobStream.on('finish', () => {
-          const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
-          images.push(publicUrl);
-          console.log(images);
-        });
-        blobStream.end(image.buffer);
-        const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
-        images.push(publicUrl);
-      }
-      return images;
-    }
-
+    storage = new Storage({ credentials });
+    bucket = storage.bucket(env.GCLOUD_STORAGE_BUCKET);
   } else {
     console.log('Não foi possível acessar o valor do segredo');
   }
+}
+
+export default async function storeImages(files: Array<Express.Multer.File>) {
+  let images: string[] = [];
+  if (!files) throw new apiError(400, "No image uploaded!");
+  for (const image of files) {
+    const blob = bucket.file(image.originalname);
+    const blobStream = blob.createWriteStream();
+    blobStream.on('error', (error: Error) => {
+      throw error;
+    });
+    blobStream.on('finish', () => {
+      const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
+      images.push(publicUrl);
+      console.log(images);
+    });
+    blobStream.end(image.buffer);
+    const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
+    images.push(publicUrl);
+  }
+  return images;
 }
 
 getSecret();
